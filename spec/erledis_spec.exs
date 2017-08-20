7e36aedff44b38185f123798c2e4eb27663c6875
Vladislav Romaniuk <vladislav.romaniuk@chisw.com>
2017-08-20 13:58:40 +0300

    write tests for unshift function

--- spec/erledis_spec.exs
+++ spec/erledis_spec.exs
@@ -16,7 +16,6 @@ defmodule ErledisSpec do
         expect(Erledis.exists?("set_2")) |> to(be_true())
         expect(Erledis.get("set_2")) |> to(eq ["word"])
         Erledis.set("set_2", [1,2,3])
-        expect(Erledis.exists?("set_2")) |> to(be_true())
         expect(Erledis.get("set_2")) |> to(eq ["word", [1,2,3]])
       end
     end
@@ -48,6 +47,31 @@ defmodule ErledisSpec do
     context "undefined element" do
       it do: expect(Erledis.get("atom")) |> to(eq [])
       it do: expect(Erledis.get("string")) |> to(eq [])
+    end
+  end
+
+  describe "unshift" do
+    context "with correct key" do
+      before do
+        Erledis.set("unshift", "word")
+      end
+
+      context "where key is defined" do
+        it do
+          expect(Erledis.unshift("unshift", 10)) |> to(eq [10, "word"])
+          expect(Erledis.unshift("unshift", {1,2,3})) |> to(eq [{1,2,3}, 10, "word"])
+        end
+      end
+
+      context "where key is undefined" do
+        it do: expect(Erledis.unshift("atom", :atom)) |> to(eq [:atom])
+        it do: expect(Erledis.unshift("tuple", {1,2,3})) |> to(eq [{1,2,3}])
+      end
+    end
+
+    context "with incorrect key" do
+      it do: expect(Erledis.unshift(1, 2)) |> to(eq "key argument must be a string")
+      it do: expect(Erledis.unshift([1], 2)) |> to(eq "key argument must be a string")
     end
   end
 
