a0462890e1bbee16e49b44cff2848b47f65c197a
Vladislav Romaniuk <vladislav.romaniuk@chisw.com>
2017-08-21 17:52:23 +0300

    call functions by name

--- lib/erledis/supervisor.ex
+++ lib/erledis/supervisor.ex
@@ -1,8 +1,8 @@
 defmodule Erledis.Supervisor do
   use Supervisor
 
-  def start_link do
-    Supervisor.start_link(__MODULE__, [:erledis])
+  def start_link(name) do
+    Supervisor.start_link(__MODULE__, [], name: name)
   end
 
   def init(_) do
