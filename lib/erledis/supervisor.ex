a202834350c76828ce6618aa9a2ef1a128a92527
Vladislav Romaniuk <vladislav.romaniuk@chisw.com>
2017-08-22 15:35:30 +0300

    rewrite Erledis with dynamic creation GenServer

--- lib/erledis/supervisor.ex
+++ lib/erledis/supervisor.ex
@@ -1,8 +1,8 @@
 defmodule Erledis.Supervisor do
   use Supervisor
 
-  def start_link(name) do
-    Supervisor.start_link(__MODULE__, [], name: name)
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], __MODULE__)
   end
 
   def init(_) do
