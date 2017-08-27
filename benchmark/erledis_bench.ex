4666970767eff06d232357eb99886f18e14ac692
Vladislav Romaniuk <vladislav.romaniuk@chisw.com>
2017-08-27 15:10:21 +0300

    some fixes in benchmarks

--- benchmark/erledis_bench.ex
+++ benchmark/erledis_bench.ex
@@ -1,11 +1,9 @@
 server = Erledis.generate_gen_server
 server.start_link
-Enum.each(Enum.to_list(1..10_000), fn(x) -> server.set("hello", x) end)
+Enum.each(Enum.to_list(1..10_000), fn(x) -> server.push("hello", x) end)
 
 Benchee.run(%{
-  "set element"           => fn -> server.set("hello", "word") end,
   "get element"           => fn -> server.get("hello") end,
-
   "push element"          => fn -> server.push("hello", "word") end,
   "pop element"           => fn -> server.pop("hello") end,
 }, time: 3)
